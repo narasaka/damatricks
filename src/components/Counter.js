a9be6d4bdf8c5a8c6d24eac7e828931267149e5c
narasaka <61931072+narasaka@users.noreply.github.com>
2021-05-17 03:00:37 -0700

    feature editable player names

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 
 const Counter = (props) => {
-	const handlePlus = () => {
-		props.setScore((score) => score + 1);
-	};
-	const handleMinus = () => {
-		if (props.score > 0) props.setScore((score) => score - 1);
-	};
+  const [name, setName] = useState(props.player);
+  const handlePlus = () => {
+    props.setScore((score) => score + 1);
+  };
+  const handleMinus = () => {
+    if (props.score > 0) props.setScore((score) => score - 1);
+  };
+  const handleChange = (target) => {
+    setName(target.value);
+  };
 
-	return (
-		<div className="counter">
-			<div className="player">{props.player}</div>
-			<div className="score">{props.score}</div>
-			<button className="btn minus" onClick={handleMinus}>
-				-
-			</button>
-			<button className="btn plus" onClick={handlePlus}>
-				+
-			</button>
-		</div>
-	);
+  return (
+    <div className="counter">
+      <input
+        className="player"
+        type="text"
+        value={name}
+        onChange={handleChange}
+      />
+      <h2 className="score">{props.score}</h2>
+      <button className="btn minus" onClick={handleMinus}>
+        -
+      </button>
+      <button className="btn plus" onClick={handlePlus}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
-
