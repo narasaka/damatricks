ce945b7331ef83807194ef9aa84ffdfabfebe0f3
narasaka <61931072+narasaka@users.noreply.github.com>
2021-05-17 00:57:43 -0700

    fix counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = (props) => {
-  const handlePlus = () => {
-    props.setScore((score) => score + 1);
-  };
-  const handleMinus = () => {
-    if (props.score > 0) props.setScore((score) => score - 1);
-  };
+	const handlePlus = () => {
+		props.setScore((score) => score + 1);
+	};
+	const handleMinus = () => {
+		if (props.score > 0) props.setScore((score) => score - 1);
+	};
 
-  return (
-    <div className="counter">
-      <div className="player">{props.player}</div>
-      <h2 className="score">{props.score}</h2>
-      <button className="btn minus" onClick={handleMinus}>
-        -
-      </button>
-      <button className="btn plus" onClick={handlePlus}>
-        +
-      </button>
-    </div>
-  );
+	return (
+		<div className="counter">
+			<div className="player">{props.player}</div>
+			<div className="score">{props.score}</div>
+			<button className="btn minus" onClick={handleMinus}>
+				-
+			</button>
+			<button className="btn plus" onClick={handlePlus}>
+				+
+			</button>
+		</div>
+	);
 };
 
 export default Counter;
+
